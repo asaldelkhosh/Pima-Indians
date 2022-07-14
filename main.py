86c80a9f2d6268327d5fae2d774b82120f173e43
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-14 11:00:19 +0430

    feat: dataset and imports

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('VIM')
+# first neural network with keras tutorial
+from numpy import loadtxt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+
+
+if __name__ == "__main__":
+    pass
